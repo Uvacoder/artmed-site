3433f27144ee0df841c593ff60dffba6070475ea
Leonardo Reis <leonardobsr@gmail.com>
2021-08-03 20:45:03 -0300

    console log debug

--- plugins/auth.js
+++ plugins/auth.js
@@ -4,18 +4,13 @@ export default ({ app }) => {
   app.$auth.$storage.watchState('user', async () => {
     const socialMedia = app.$auth.$storage.getLocalStorage('strategy')
     if (socialMedia === 'facebook') {
-      if (app.$auth.loggedIn) {
-        alert(`token ${app.$auth}`)
-        alert(`token ${app.$auth.strategy.token.get()}`)
-        const endpoint = app.$api.EndPoints.socialLogin
-        const params = {
-          token: app.$auth.strategy.token.get(),
-          source: socialMedia
-        }
-        await app.$api.request(endpoint, params)
-      } else {
-        app.$auth.logout()
+      alert(`token ${app.$auth.strategy.token.get()}`)
+      const endpoint = app.$api.EndPoints.socialLogin
+      const params = {
+        token: app.$auth.strategy.token.get(),
+        source: socialMedia
       }
+      await app.$api.request(endpoint, params)
     }
   })
 }
