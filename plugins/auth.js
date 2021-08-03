40ff64a7059a6b7a6d8212d30822614a70cd375b
Leonardo Reis <leonardobsr@gmail.com>
2021-08-03 20:17:27 -0300

    console log debug

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,7 +1,7 @@
 export default ({ app }) => {
   // Only _actual_ login/outs (including resets) will be watched here.
   console.log('Registering watch state in plugin!')
-  app.$auth.$storage.watchState('loggedIn', () => {
+  app.$auth.$storage.watchState('user', () => {
     alert(app.$auth.$storage.getLocalStorage('strategy'))
     // if (userNew) {
     //   // Follow @nuxtjs/auth workflow.
